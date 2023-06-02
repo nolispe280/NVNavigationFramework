53a69eb1989e5ca7d986127110345647fb612000
dev-axel <axel@n-vibe.fr>
2023-06-02 18:27:40 +0200

    First commit

---
+++ NVNavigationKit.framework/Headers/NVNavigationKit.h
@@ -0,0 +1,18 @@
+//
+//  NVNavigationKit.h
+//  NVNavigationKit
+//
+//  Created by MAC PRO N-VIBE on 26/05/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NVNavigationKit.
+FOUNDATION_EXPORT double NVNavigationKitVersionNumber;
+
+//! Project version string for NVNavigationKit.
+FOUNDATION_EXPORT const unsigned char NVNavigationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NVNavigationKit/PublicHeader.h>
+
+
